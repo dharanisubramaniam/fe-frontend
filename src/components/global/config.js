141409689f3527f0fd0c8d76e03e68a81817aaf2
Dharani Subramaniam <dharani2597@gmail.com>
2022-09-15 12:52:10 +0530

    changes in config

--- src/components/global/config.js
+++ src/components/global/config.js
@@ -1,5 +1,5 @@
 export const perPage = 6;
 export const localURL = "http://localhost:3000";
 export const mainURL = "https://www.oerrors.com";
-export const baseURL = "http://localhost:5001";
+export const baseURL = "https://filmy-effects-backend.herokuapp.com/";
 // process.env.NODE_ENV === "development" ? localURL : mainURL;
